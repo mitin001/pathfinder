011d3aec9eef69081ce7a173fa2c416d85e6a58e
Andrey Mitin <amitin@appdataroom.com>
2017-04-09 16:10:57 -0500

    Bootstrap an EC2 instance with yum and rpm by installing git and using it to clone this repository

--- ec2/bootstrap.sh
+++ ec2/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-yum -y install mysql
+yum -y install mysql git
 curl -s ftp://rpmfind.net/linux/dag/redhat/el5/en/x86_64/dag/RPMS/bbe-0.2.2-1.el5.rf.x86_64.rpm \
 	-o bbe-0.2.2-1.el5.rf.x86_64.rpm
 rpm -Uvh bbe-0.2.2-1.el5.rf.x86_64.rpm
+git clone https://github.com/mitin001/pathfinder.git
